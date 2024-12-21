05281b711102e1739d558a9fbc3db69aff19c782
Banshita-Bansal <app5.nessco@gmail.com>
2024-12-21 14:31:41 +0530

    backend

--- credentials.js
+++ credentials.js
@@ -1,9 +1,9 @@
-import { writeFileSync } from 'fs';
+const fs = require('fs');
 
 // Decode the Base64 string
 const googleCredentials = Buffer.from(process.env.GOOGLE_CREDENTIALS, 'base64').toString('utf8');
 
 // Write the decoded credentials to a temporary file
-writeFileSync('credentials.json', googleCredentials);
+fs.writeFileSync('credentials.json', googleCredentials);
 
 console.log('credentials.json created successfully.');
